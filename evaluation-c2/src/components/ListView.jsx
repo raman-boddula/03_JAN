046bf70a63a022eabcafe37830637023a34a0828
Raman Boddula <raman_rc001@yahoo.com>
2022-01-03 15:41:30 +0530

    3

---
+++ evaluation-c2/src/components/ListView.jsx
@@ -0,0 +1,9 @@
+import "./listView.css"
+export const ListView = () => {
+    return (
+        <div className="mdiv">
+            <div>Title</div>
+            <div>Time</div>
+        </div>
+    )
+}
--- evaluation-c2/src/components/Parent.jsx
+++ evaluation-c2/src/components/Parent.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { ReceipeInput } from "./RecepeInput";
-
- 
+import { ListView } from "./ListView";
+import { ListViewData} from "./listViewData"
 export const Parent = () => {
     const [data, setData] = React.useState([]);
-    
+
+    React.useEffect(() => {
+        fetch("http://localhost:3333/receipeData").then(d => d.json()).then(d => setData(d));
+    }, []);
+    console.log("data",data)
     return (
         <>
-        < ReceipeInput />
+            < ReceipeInput />
+            {data.length > 1 ? <ListView />:null}
+            {data.map((el) => <ListViewData key={el.id}{...el} />)}
         </>
-    )
+    );
 
 } 
