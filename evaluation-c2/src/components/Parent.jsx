9817d67c02d5c73b7fa7c5d79ac8df4238d50cca
Raman Boddula <raman_rc001@yahoo.com>
2022-01-03 17:19:25 +0530

    "done"

--- evaluation-c2/src/components/Parent.jsx
+++ evaluation-c2/src/components/Parent.jsx
@@ -7,21 +7,25 @@ import {Display} from "./ScreenDisplay"
 export const Parent = (id) => {
     const [data, setData] = React.useState([]);
     const [screen, setScreenData] = React.useState(null);
+    const [sorted, setSorted] = React.useState("asc");
     const dummyFunc = (id) => {
         fetch(`http://localhost:3333/receipeData/${id}`).then(d => d.json()).then(d => setScreenData(d));
     }
     React.useEffect(() => {
-        fetch("http://localhost:3333/receipeData").then(d => d.json()).then(d => setData(d));
-    }, [screen]);
-    console.log("data",data)
+        fetch(`http://localhost:3333/receipeData?_sort=timetocook&_order=${sorted}`).then(d => d.json()).then(d => setData(d));
+    }, [screen,sorted]);
+    console.log("data", data)
     return (
         <div className="mainDiv">
             <div>
             < ReceipeInput />
             </div>
             <div >
-            {data.length > 1 ? <ListView />:null}
+                <button onClick={()=>setSorted("asc")}>Sort by Less Cooking Time</button>
+                <button onClick={()=>setSorted("desc")}>Sort by More Cooking Time</button>
+                {data.length > 1 ? <ListView /> : null}
             {data.map((el) => <ListViewData key={el.id}{...el} dummyFunc={dummyFunc}/>)}
+            
             </div>
             <div>
             < Display {...screen} />
