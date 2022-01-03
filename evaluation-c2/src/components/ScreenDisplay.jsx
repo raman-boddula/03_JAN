1cbd1b45bb5d9b2a1845e08ad3359ec8816e8781
Raman Boddula <raman_rc001@yahoo.com>
2022-01-03 17:06:57 +0530

    6

--- evaluation-c2/src/components/ScreenDisplay.jsx
+++ evaluation-c2/src/components/ScreenDisplay.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-
-export const Display = ({ title,ingredients,dummyFunc}) => {
+import './listView.css'
+export const Display = ({ instructions,timetocook,title,ingredients,dummyFunc,image}) => {
     return (
         <div className="mdiv1">
             <div>
             {title}
-
             </div>
             <div>
             {ingredients}
-
             </div>
+            <div>{ instructions}</div>
+            <div>{ timetocook} Minutes</div>
+            <div>{image}</div>
         </div>
     )
 }
